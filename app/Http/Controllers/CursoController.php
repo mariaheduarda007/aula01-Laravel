d4e5f408ef0d420cf7709141de730f46ce3fa039
MariaEduarda-git <mariah.bsc18@gmail.com>
2025-11-04 16:22:59 -0300

    View de curso e create pronto

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -13,4 +13,20 @@ class CursoController extends Controller
         $cursos = Curso::all();
         return view("curso.index", compact('cursos'));
     }
+
+    public function create()
+    {
+        $cursos = Curso::all();
+        return view('curso.create', compact('cursos'));
+    }
+
+    public function store(Request $request)
+    {
+        $curso = new Curso();
+        $curso->nome = mb_strtoupper($request->nome, 'UTF-8');
+        $curso->duracao = $request->duracao;
+        $curso->save();
+        return redirect()->route('curso.index');
+    }
+
 }
